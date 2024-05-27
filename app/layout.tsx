fc990555f8d32429622b29b2a2ebfb57ef5b81ff
boaloysius <boaloysius@gmail.com>
2024-05-27 07:06:35 +0530

    Add font and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,17 +6,49 @@ import {
   UserButton,
   SignUpButton,
 } from "@clerk/nextjs";
+
+import { Inter, Space_Grotesk } from "next/font/google";
+
+import type { Metadata } from "next";
 import "./globals.css";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "CommunityApp",
+  description: "A community project to interact with peers.",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           <SignedOut>
             <SignInButton />
             <SignUpButton />
